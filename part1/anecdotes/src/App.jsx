c686ed802c844975b85ede9e466bca7ff1bd6553
worhol <trashuries@gmail.com>
2023-12-06 19:50:23 -0600

    Completed exercises 1.12-1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+  });
+  const getMaxKey = (arr) => {
+    let max = 0;
+    let maxKey = 0;
+    Object.entries(arr).forEach(([key, value]) => {
+      if (value > max) {
+        max = value;
+        maxKey = key;
+      }
+    });
+    return maxKey;
+  };
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <br />
+
+      <button
+        onClick={() =>
+          setVotes({ ...votes, [selected]: (votes[selected] += 1) })
+        }
+      >
+        vote
+      </button>
+      <button onClick={() => setSelected(Math.floor(Math.random() * 8))}>
+        next anecdote
+      </button>
+      <h2>Anecdote with most votes</h2>
+      {anecdotes[getMaxKey(votes)]}
+      <p>has {votes[getMaxKey(votes)]} votes</p>
+    </div>
+  );
+};
+
+export default App;
