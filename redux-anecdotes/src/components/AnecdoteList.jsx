02593731d4695d7ce467d6f974404266ac269cce
worhol <trashuries@gmail.com>
2024-01-20 21:21:13 -0600

    Implemented filtering for the anecdotes that are displayed to the user.

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,15 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
-
+import Filter from './Filter'
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
   const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => {
+    if (state.filter === 'ALL') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+    )
+  })
 
   const vote = (id) => {
     dispatch(addVote(id))
   }
   return (
     <div>
+      <Filter />
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
